f05e8f93ed60eb9db70455adedbfd657b395f19b
andersontecnicoprogramador <68762932+andersontecnicoprogramador@users.noreply.github.com>
2022-05-28 00:52:47 -0300

    Add files via upload

---
+++ Aulas_javascript/js/main.js
@@ -0,0 +1,23 @@
+
+
+var fruta = {nome:"maça",cor: "vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);
+
+//var lista = ["maça","pêra","laranja"];
+//lista.push("uva");
+//lista.pop();
+//console.log(lista);
+
+
+
+//var nome = "Anderson Moreira";
+//var idade = 39;
+//var idade2 = 10;
+//var frase = "japão é melhor time do mundo";
+//alert(nome + "tem" + idade + "anos");
+//alert(idade + idade2);
+//console.log(nome);
+//console.log(idade * idade2);
+//onsole.log(frase.replace("japão","Brasil"));
+//alert(frase.replace("japão","Brasil"));
